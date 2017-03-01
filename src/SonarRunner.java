2bd5042fb26544cf7e0ebae65868f9b631f80a28
Alexandru Stan <AlexandruCristian.Stan@stud-inf.unibz.it>
2017-03-01 16:16:13 +0100

    1st

---
+++ src/SonarRunner.java
@@ -0,0 +1,21 @@
+/**
+ * Created by astan on 3/1/2017.
+ *
+ *
+ * Usage: run the SonarRunner script to extract data
+ *
+ * Procedure:
+ * 01) provide the home directory where SonarRunner is located
+ *
+ *
+ *
+ */
+public class SonarRunner {
+
+    
+
+
+
+
+
+}
